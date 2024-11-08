c945f1a10e93405cc91ef7cdbe33a11cec69db28
root <root@LAB10-04.localdomain>
2024-11-07 21:16:07 -0300

    feat: adição do botão de acesso

--- engineering-page/src/app/components/header-fecaf/HeaderFecafLogin.tsx
+++ engineering-page/src/app/components/header-fecaf/HeaderFecafLogin.tsx
@@ -1,13 +1,11 @@
 import styles from './HeaderFecafLogin.module.css'
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-
 
 const HeaderFecafLogin = () => {
     return (
         <div className={styles.container}>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"/>
             <img className={styles.img} src='https://www.unifecaf.com.br/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo-unifecaf.77c64349.png&w=640&q=75'/>
-        </div>
+            <a target="_blank" className={styles.a} href="http://aluno.unifecaf.edu.br/"> <i className="fa fa-user"></i>Acesso</a></div>
     )
 }
 export default HeaderFecafLogin;
